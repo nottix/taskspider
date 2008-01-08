5caa081df005a82502ef2954974d891807233aea
notargiacomo.s <notargiacomo.s@33f12823-ba41-0410-9171-03d572cf252a>
2008-01-08 18:08:53 +0000

    Aggiunta classe per leggere il file di configurazione e apportate modifiche alla classe Spider

---
+++ trunk/TaskSpider/src/taskspider/util/properties/PropertiesReader.java
@@ -0,0 +1,46 @@
+/**
+ * @author Simone Notargiacomo, Giuseppe Schipani
+ */
+package taskspider.util.properties;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author Simone Notargiacomo, Giuseppe Schipani
+ *
+ */
+public class PropertiesReader {
+	private static Properties properties = null;
+	private static File file = null;
+	private static FileInputStream fis = null;
+	private static String ret = null;
+	
+	public static String getProperty(String id) {
+		try {
+			if(id=="" || id==null)
+				return null;
+			if(file==null) {
+				file = new File(System.getProperty("user.dir")+"/conf/config.properties");
+				fis = new FileInputStream(file);
+			}
+			if(properties==null) {
+				properties = new Properties();
+				properties.load(fis);
+				fis.close();
+			}
+
+			if((ret=properties.getProperty(id))!=null)
+				return ret;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
