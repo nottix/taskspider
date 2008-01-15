5b332f8459c350da66c2a14207ef218de33caf9a
notargiacomo.s <notargiacomo.s@33f12823-ba41-0410-9171-03d572cf252a>
2008-01-15 22:16:32 +0000

    Aggiunto Indexer su filesystem

--- TaskSpider/src/taskspider/spider/testing/Testing.java
+++ TaskSpider/src/taskspider/spider/testing/Testing.java
@@ -20,11 +20,12 @@ import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.Hits;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
-import org.apache.lucene.store.LockObtainFailedException;
-import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.store.*;
 
 import taskspider.spider.core.*;
+import taskspider.util.properties.*;
 import taskspider.data.document.*;
+import taskspider.retrival.core.*;
 
 /**
  * @author Simone Notargiacomo, Giuseppe Schipani
@@ -36,17 +37,17 @@ public class Testing {
 	 */
 	public static void main(String[] args) {
 		try {
-			Link[] links = { new Link("http://www.maglificiosalerno.it"), new Link("http://www.alessioluffarelli.it")/*, new Link("http://www.google.com"), new Link("http://www.ibm.com")*/ };
+			Link[] links = { /*new Link("http://www.maglificiosalerno.it"),*/ new Link("http://www.alessioluffarelli.it")/*, new Link("http://www.google.com"), new Link("http://www.ibm.com")*/ };
 			Spider spider = new Spider(links);
 			spider.setMaxLevel(3);
 			spider.start();
 			SpiderExplorer spiderExplorer = new SpiderExplorer(spider);
 			spiderExplorer.start();
-			//addDocs(spiderExplorer.getDocs());
-			//System.out.println(spiderExplorer.getDocs().toString());
+			Indexer indexer = new Indexer("alessioluffarelli");
 			Vector<Document> docs = spiderExplorer.getDocs();
 			int start = 0;
 			int end = docs.size();
+			indexer.indexDocs(docs, start, end);
 			int retry=0;
 			while(start!=end || retry<4) {
 				if(start==end)
@@ -54,70 +55,32 @@ public class Testing {
 				else
 					retry=0;
 				System.out.println("start: "+start+", end: "+end);
-				addDocs(spiderExplorer.getDocs(), start, end);
-				Thread.sleep(1000);
+				
+				try {
+					if(indexer.search("Alessio", "title")!=0) {
+						for(int i=0; i<indexer.getResult().length(); i++) {
+							System.out.println("RES: "+indexer.getResult().doc(i).get("url"));
+						}
+					}
+				} catch (CorruptIndexException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				indexer.indexDocs(spiderExplorer.getDocs(), start, end);
+				Thread.sleep(2000);
 				start = end;
 				end = docs.size();
 			}
 			spiderExplorer.interrupt();
 			System.out.println("STOPPED "+start+" "+end);
+			indexer.getDocument();
 			
 		}
 		catch(MalformedURLException ex) {ex.printStackTrace();}
 		catch(InterruptedException ex1) {ex1.printStackTrace();}
 	
-	}
-	
-	public static void addDocs(Vector<Document> docs, int start, int end) {
-		try {
-//			 To store an index on disk, use this instead (note that the 
-		    // parameter true will overwrite the index in that directory
-		    // if one exists):
-		    // Directory directory = FSDirectory.getDirectory("/tmp/testindex", true);
-			//Thread.sleep(2000);
-			RAMDirectory idx = new RAMDirectory();
-			IndexWriter indexWriter = new IndexWriter(idx, new StandardAnalyzer(), true);
-			for(int i=start; i<end; i++) {
-				//System.out.println("URL: "+docs.get(i).get("url"));
-				indexWriter.addDocument(docs.get(i));
-			}
-			indexWriter.optimize();
-			indexWriter.close();			
-			
-//			 Now search the index:
-		    IndexSearcher isearcher = new IndexSearcher(idx);
-
-		    // Parse a simple query that searches for "text":
-		    QueryParser parser = new QueryParser("title", new StandardAnalyzer());
-		    Query query = parser.parse("Alessio");
-		    Hits hits = isearcher.search(query);
-		    //assertEquals(1, hits.length());
-		    System.out.println("Hits: "+hits.length());
-		    // Iterate through the results:
-		    for (int i = 0; i < hits.length(); i++)
-		    {
-		      Document hitDoc = hits.doc(i);
-		      System.out.println("Res: "+hitDoc.get("url"));
-		      //assertEquals("This is the text to be indexed.", hitDoc.get("fieldname"));
-		    }
-
-		    isearcher.close();
-		    idx.close();
-		} catch (CorruptIndexException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (LockObtainFailedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} /*catch (InterruptedException e){
-			
-		}*/
 	}
 
 }
