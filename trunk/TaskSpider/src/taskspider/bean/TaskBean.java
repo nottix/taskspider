a30d7bf78397a1263faf2fb21bc4153e86dc27c4
notargiacomo.s <notargiacomo.s@33f12823-ba41-0410-9171-03d572cf252a>
2008-01-30 23:45:57 +0000

    Aggiunte funzionalità alla pagina jsp

--- trunk/TaskSpider/src/taskspider/bean/TaskBean.java
+++ trunk/TaskSpider/src/taskspider/bean/TaskBean.java
@@ -12,9 +12,13 @@ import org.apache.lucene.search.Hits;
 
 public class TaskBean{
 	
-	private Vector<Document> results = null;
-	private Hits hits = null;
-	private Controller controller = null;
+	private static Vector<Document> results = null;
+	private static Hits hits = null;
+	private static Controller controller = null;
+	private static String task = "";
+	private static String query = "";
+	private static int first = 0; 
+	private static int start, end, size;
 	
 	//costruttore,  inutile ma serve a tomcat, altrimenti crea errore
 	public TaskBean(){}
@@ -23,14 +27,21 @@ public class TaskBean{
 	 * Do in input i parametri task e query, Qui dovresti fare l'operazione de
 	 * taskspider e poi riempirmi due vettori.
 	 */
-	public String doSearch(String task, String query){
+	public static String doSearch(String taskSearch, String querySearch){
 		if(controller==null)
 			controller = new Controller();
 		
+		if(taskSearch.equals(task) && querySearch.equals(query)) {
+			first++;
+			return "0";
+		}
+		first = 0;
 		int ret;
-		if((ret=controller.search(task, query))>0) {
+		if((ret=controller.search(taskSearch, querySearch))>0) {
+			task = taskSearch;
+			query = querySearch;
 			try {
-				Thread.sleep(4000);
+				Thread.sleep(2000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -43,14 +54,10 @@ public class TaskBean{
 		
 	}
 	
-	public String toString() {
-		return "OK";
-	}
-	
 	/*
 	 * Non ricordo a che ci serviva....
 	 */
-	public void doCancel(){
+	public static void doCancel(){
 		controller.interrupt();
 	}
 	
@@ -58,51 +65,84 @@ public class TaskBean{
 	 * Con questo prendo un vettore di risultati e lo stampo a centro 
 	 * della pagina. Non paginiamo, li stampiamo tutti in sequenza.
 	 */
-	public Vector<Document> getResults(){
+	public static Vector<Document> getResults(String indexString){
+		int index = Integer.parseInt(indexString);
 		if(controller==null)
 			return new Vector<Document>();
-		hits = controller.getResult();
-		Vector<Document> res = new Vector<Document>();
-		
-		try {
-			for(int i=0; i<hits.length(); i++) {
-				res.add(hits.doc(i));
+		if(first==0) {
+			hits = controller.getResult();
+			results = new Vector<Document>();
+			size = hits.length()/4;
+			
+			try {
+				for(int i=0; i<4; i++) {
+					results.add(hits.doc(i));
+				}
+			} catch (CorruptIndexException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} catch (CorruptIndexException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		}
+		else {
+			start = index*4;
+			end = start+4;
+			if(end<=hits.length()) {
+				results = new Vector<Document>();
+				try {
+					for(int i=start; i<end; i++) {
+						results.add(hits.doc(i));
+					}
+				} catch (CorruptIndexException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		
-		return res;
+		return results;
 	} 
 	
 	/*
 	 * Questo mi da i documenti per l'albero, purtoppo non lo posso provare
 	 * perch non riesco ad avere il sitema funzionante sul server tomcat
 	 */
-	public Vector<Document> getTree(){
-//		Vector<Document> V = new Vector<Document>();
-//		Document D = new Document();
-//		D.add(new Field("url",null));
-//		D.add(new Field("url","http://www.sito1.it/index.php"));
-//		D.add(new Field("url","http://www.sito1.it/altrapagina.php"));
-//		D.add(new Field("url",null));
-//		D.add(new Field("url","http://www.sito2.it/simone.php"));
-//		D.add(new Field("url","http://www.sito2.it/giuseppe.html"));
-//		D.add(new Field("url",null));
-//		D.add(new Field("url","http://www.sito3.it/"));
-//		D.add(new Field("url","http://www.sito3.it/pippo.jsp"));
-//		D.add(new Field("url",null));
+	public static Vector<Document> getTree(){
 		if(controller==null) {
 			Vector<Document> docs = new Vector<Document>();
 			docs.add(new Document());
 			return docs;
 		}
-		results = (Vector<Document>)controller.getGroupResult();
-		
-		return this.results;
+		return (Vector<Document>)controller.getGroupResult();
 	} 
+	
+	public static String getArg(String name, String queryString) {
+		System.out.println("QUERYSTRING: "+queryString+", name: "+name);
+		
+		int start = queryString.indexOf(name)+name.length()+1;
+		if(start<0)
+			return null;
+		int end = queryString.indexOf("&", start);
+		if(end<0)
+			end = queryString.length()-1;
+		
+		System.out.println("NAME: "+queryString.substring(start, end));
+			
+		return queryString.substring(start, end);
+	}
+	
+	public static String printTail(String indexString) {
+		int index = Integer.parseInt(indexString);
+		String ret = "";
+		for(int i=0; i<size; i++) {
+			if(index==i)
+				ret += "<a class=\"description\"> "+(i+1)+" </a>";
+			else
+				ret += "<a href=\"http://localhost:8180/taskspider/index.jsp?task="+task+"&query="+query+"&frame=1&index="+i+"&\" class=\"description\"> "+(i+1)+" </a>";
+		}
+		return ret;
+	}
 
 
 }
